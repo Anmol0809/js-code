5370e4dfe9f6f92e0dabf385a3ac56775edb7923
Anmol kumar <122743025+Anmol0809@users.noreply.github.com>
2024-02-18 17:49:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 2111981040
+let accountEmail = "anmol1040.be21@chitkarauniversity.edu.in"
+var accountPassword = "12345"
+    accountCity = "Ferozepur"
+let accountState 
+
+   
+    accountEmail = "anmol1040.be21@chitkarauniversity.edu.in"
+    accountPassword = "23456"
+    accountCity = "Firozepur"
+
+    /* prefer not use var because of block scope and functional scope */
+console.table([accountId,accountEmail,accountPassword,accountCity , accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code newer version
+
+// alert(3+3); //we are using nodejs, not browser 
+console.log(3+3);   //code readability should be high
+console.log("Anmol");
+
+let age = 18
+let isLoggedIn = false
+let state;  
+ 
+
+//----------------PRIMITVE-- DATATYPES-------------
+//number => 2 to power 53
+//bigInt
+//string =>  "" 
+//boolean => true/false
+//null => standalone value
+// undefined => not assign any value
+// symbol => to find uniqueness
+
+
+//----------------OBJECT-------------------
+console.log(typeof true);
