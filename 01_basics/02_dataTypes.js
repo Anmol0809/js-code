af9a8b6068c50d7651382499238ed7c5a6cb2c85
Anmol kumar <122743025+Anmol0809@users.noreply.github.com>
2024-02-28 01:17:19 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,7 +1,7 @@
 "use strict"; // treat all JS code newer version
 
 // alert(3+3); //we are using nodejs, not browser 
-console.log(3+3);   //code readability should be high
+console.log(3+3); //code readability should be high
 console.log("Anmol");
 
 let age = 18
