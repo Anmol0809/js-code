e8496940ec8998f1d6d3ce02b138a41529a39ec6
Anmol kumar <122743025+Anmol0809@users.noreply.github.com>
2024-02-29 12:27:08 +0000

    ConversionOfDatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score  = "hitesh";
+
+// console.log(typeof(score));
+// console.log(typeof score);
+// console.log(score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let val = null;
+// console.log(val);
+
+//--------notes of coversion of string -------------------------
+// "33" => 33
+// "33abc" => NaN(not a number) ,but type is number
+// true => 1; false => 0;
+
+let isLoggedIn = -999;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//---------Notes------------
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => false
+// "-999 to 999" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
